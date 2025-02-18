f87a5b6d9e29336fd6602fda08d4c192c0ab7761
Aryan Pandey <rtp.aryan@gmail.com>
2025-02-18 18:51:20 +0530

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,7 @@
+        confirm("Please enter your name")
+
+      var c = prompt("Enter your name:")
+      alert("Hello" +" "+c+" "+ "Please Login this page")
+        
+            document.getElementById("User").innerText = c;
+       
